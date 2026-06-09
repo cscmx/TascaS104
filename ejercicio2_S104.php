be477d7d08e8c27050cdcf2e968c138200831a9f
cscmx <sainz.carolina@gmail.com>
2026-06-09 13:07:51 +0200

    Refactor: require_once de todas las clases en el main

--- ejercicio2_S104.php
+++ ejercicio2_S104.php
@@ -2,50 +2,15 @@
 /*
 Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea amb un mètode.
 */
-class Shape 
-{
-    public int $alto;
-    public int $ancho;
 
-    public function __construct(int $alto, int $ancho){
-        $this->alto = $alto;
-        $this->ancho = $ancho;
-    }
-    public function getAlto (){
-        return $this->alto;
-    }
-    public function getAncho (){
-        return $this->ancho;
-    }
-    public function setAlto(int $alto){
-        $this->alto = $alto;
-    }
-    public function setAncho(int $ancho){
-        $this->ancho = $ancho;
-    }
-
-}
-
-class Triangle extends Shape 
-{
-    public function calcularArea(){
-        return ($this->alto * $this->ancho)/2;
-    }
-}
-
- class Rectangle extends Shape 
- {
-    public function calcularArea() {
-        return $this->alto * $this->ancho;
-    }
- }
-
- 
+require_once 'classShape.php';
+require_once 'classTriangle.php';
+require_once 'classRectangle.php';
  
  $myTriangle = new Triangle(10,5);
  $myRectangle = new Rectangle (5, 8);
 
- echo "Area triangulo: ".$myTriangle->calcularArea().PHP_EOL;
- echo "Area rectángulo: ".$myRectangle->calcularArea().PHP_EOL;
+ echo "Area triangulo: ".$myTriangle->calculateArea().PHP_EOL;
+ echo "Area rectángulo: ".$myRectangle->calculateArea().PHP_EOL;
 
  ?>
